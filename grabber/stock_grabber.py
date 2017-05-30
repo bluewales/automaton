d13fd3e4b904498b4c3174164cef47c17d280172
Luke <luked@owletcare.com>
2017-05-30 17:12:45 -0600

    Added stock data cache

--- grabber/stock_grabber.py
+++ grabber/stock_grabber.py
@@ -1,29 +1,98 @@
 import requests
+import datetime
+import json
+from datetime import timedelta, date
+from dateutil.relativedelta import relativedelta
 
 
-example_url = "http://www.google.com/finance/historical?q=GOOG&histperiod=daily&startdate=Apr+1+2014&enddate=Apr+15+2014&output=csv"
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
 
-base_url = "http://www.google.com/finance/historical?q={}&histperiod=daily&startdate={}+{}+{}&enddate={}+{}+{}&output=csv"
 
+class StockGrabber:
+    def __init__(self):
+        self.example_url = "http://www.google.com/finance/historical?q=GOOG&histperiod=daily&startdate=Apr+1+2014&enddate=Apr+15+2014&output=csv"
+        self.base_url = "http://www.google.com/finance/historical?q={}&histperiod=daily&startdate={}&enddate={}&output=csv"
+        self.cached_data = {}
 
+    def fetch_stock_data(self, ticker, day, field):
+
+        day_string = day.strftime("%d-%b-%y")
+
+        print(day_string)
+
+        if day > datetime.date.today():
+            return None
+
+        if not (ticker in self.cached_data and day_string in self.cached_data[ticker]):
+
+            start_day = day
+            end_day = day + relativedelta(years=1)
+
+            if start_day > datetime.date.today():
+                start_day = datetime.date.today()
+            if end_day > datetime.date.today():
+                end_day = datetime.date.today()
+
+            start_day_string = start_day.strftime("%d+%b+%Y")
+            end_day_string = end_day.strftime("%d+%b+%Y")
+            url = self.base_url.format(ticker, start_day_string, end_day_string)
+            print("FETCH: " + url)
+            r = requests.get(url)
+            lines = r.text.split("\n")
+            data = []
+            for l in lines:
+                data.append(l.split(","))
+
+            if ticker not in self.cached_data:
+                self.cached_data[ticker] = {}
+
+            header = data[0]
+
+            for d in data[1:]:
+
+                if len(d) < len(header):
+                    continue
+
+                self.cached_data[ticker][d[0]] = {}
+                for i in range(1, len(header)):
+                    self.cached_data[ticker][d[0]][header[i]] = d[i]
+
+            for single_date in daterange(start_day, end_day):
+                single_day_string = single_date.strftime("%d-%b-%y")
+                if single_day_string not in self.cached_data[ticker]:
+                    self.cached_data[ticker][single_day_string] = "Closed"
+
+        if self.cached_data[ticker][day_string] == "Closed":
+            return None
+        return self.cached_data[ticker][day_string][field]
 
 
 def main():
 
     ticker = "GOOG"
+    day = date(2018, 1, 1)
+    field = "Open"
 
-    start_month = "Jan"
-    start_day = 15
-    start_year = 2017
+    sg = StockGrabber()
 
-    end_month = "Apr"
-    end_day = 17
-    end_year = 2017
+    n = sg.fetch_stock_data(ticker, day, field)
+    print(ticker + " " + str(day) + " " + field + " " + str(n))
 
-    url = base_url.format(ticker, start_month, start_day, start_year, end_month, end_day, end_year)
-    print(url)
-    r = requests.get(url)
-    print(r.text)
+    day = date(2017, 1, 1)
+
+    n = sg.fetch_stock_data(ticker, day, field)
+    print(ticker + " " + str(day) + " " + field + " " + str(n))
+
+    day = date(2017, 2, 10)
+
+    n = sg.fetch_stock_data(ticker, day, field)
+    print(ticker + " " + str(day) + " " + field + " " + str(n))
+
+
+
+
 
 
 if __name__ == "__main__":
--- grabber/variables.py
+++ grabber/variables.py
@@ -1,9 +1,12 @@
 import datetime
+from grabber.stock_grabber import StockGrabber
 
 
 class Variables:
 
     def __init__(self):
+
+        self.sg = StockGrabber()
 
 
         self.variables = [
@@ -11,25 +14,31 @@ class Variables:
                 'name': "day_of_week",
                 'description': "the day of the week, a number 0-6",
                 'type': "calendar",
-                'fetcher': lambda date: date.weekday()
+                'fetcher': lambda day: day.weekday()
             },
             {
                 'name': "day",
                 'description': "the day part of the date",
                 'type': "calendar",
-                'fetcher': lambda date: date.day
+                'fetcher': lambda day: day.day
             },
             {
                 'name': "month",
                 'description': "the month part of the date",
                 'type': "calendar",
-                'fetcher': lambda date: date.month
+                'fetcher': lambda day: day.month
             },
             {
                 'name': "year",
                 'description': "the year part of the date",
                 'type': "calendar",
-                'fetcher': lambda date: date.year
+                'fetcher': lambda day: day.year
+            },
+            {
+                'name': "GOOG Open",
+                'description': "opening price of google stock",
+                'type': "stock",
+                'fetcher': lambda day: self.sg.fetch_stock_data("GOOG", day, "Open")
             }
         ]
 
