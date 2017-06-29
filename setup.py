ab3cbf5e0be8ad5020243a65e73f65ded1e0280c
Luke <luked@owletcare.com>
2017-06-28 20:40:23 -0600

    Reqs

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ It is intended that this should work on windows, linux, and osx computers.
 """
 from subprocess import call
 from sys import platform
+import os
 
 # Example
 if __name__ == '__main__':
@@ -20,8 +21,8 @@ if __name__ == '__main__':
     call(pip + ' install wheel', shell=True)
 
     if platform == 'win32':
-        call(pip + ' install mysqlclient-1.3.9-cp35-cp35m-win_amd64.whl', shell=True)
-        call(pip + ' install mysqlclient-1.3.9-cp35-cp35m-win32.whl', shell=True)
+        call(pip + ' install reqs\mysqlclient-1.3.9-cp35-cp35m-win_amd64.whl', shell=True)
+        call(pip + ' install reqs\mysqlclient-1.3.9-cp35-cp35m-win32.whl', shell=True)
     else:
         call(pip + ' install mysqlclient', shell=True)
 
@@ -32,7 +33,17 @@ if __name__ == '__main__':
     call(pip + ' install pandas', shell=True)
 
     call(pip + ' install neat-python')
+    call(pip + ' install graphviz')
+    call(pip + ' install --upgrade tensorflow')
+    call(pip + ' install matplotlib')
 
     call(pip + ' install --upgrade tensorflow')
+    call(pip + ' install requests')
 
     call(pip + ' install flask')
+    if platform == 'win32':
+        local_dir = os.path.dirname(__file__)
+        msi_path = os.path.join(local_dir, "reqs\graphviz-2.38.msi")
+        print(msi_path)
+        os.startfile(os.path.normpath(msi_path))
+
